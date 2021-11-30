789afa3a4feb68941f7eddeebfe7a9831e63688d
Gururaja <gururaja.195@gmail.com>
2021-11-30 14:08:39 +0530

    Accounts get request for userid

--- routes/useraccounts.js
+++ routes/useraccounts.js
@@ -3,13 +3,13 @@ const express = require("express");
 const auth = require("../middleware/auth");
 const router = express.Router();
 
-router.get("/", auth, async (req, res) => {
-  const userAccounts = await UserAccount.find();
-  res.send(userAccounts);
-});
+// router.get("/", auth, async (req, res) => {
+//   const userAccounts = await UserAccount.find();
+//   res.send(userAccounts);
+// });
 
-router.get("/:id", auth, async (req, res) => {
-  const userAccount = await UserAccount.findById(req.params.id);
+router.get("/:userid", auth, async (req, res) => {
+  const userAccount = await UserAccount.find({ userid: req.params.userid });
   if (!userAccount)
     return res.status(404).send("No Account found with given ID");
 
@@ -57,12 +57,12 @@ router.put("/:id", auth, async (req, res) => {
   res.send(userAccount);
 });
 
-router.delete("/:id", auth, async (req, res) => {
-  const userAccount = await UserAccount.findByIdAndRemove(req.params.id);
-  if (!userAccount)
-    return res.status(404).send("No Account found with given ID");
+// router.delete("/:id", auth, async (req, res) => {
+//   const userAccount = await UserAccount.findByIdAndRemove(req.params.id);
+//   if (!userAccount)
+//     return res.status(404).send("No Account found with given ID");
 
-  res.send(userAccount);
-});
+//   res.send(userAccount);
+// });
 
 module.exports = router;
